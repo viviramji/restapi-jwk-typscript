76e57bd197cb3f602dc6cb88c3003f684f9386a9
viviramji wsl <viviramji@gmail.com>
2020-08-17 01:45:04 -0500

    Encrypt user password

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import { model, Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
 
 export interface IUser extends Document {
   email: string;
@@ -18,5 +19,29 @@ const schema = new Schema({
     required: true,
   },
 });
+
+// * before save (pre save)
+
+schema.pre<IUser>('save', async function(nextCallback) {
+    const user = this;
+    if (!user.isModified('password')) return nextCallback();
+
+    // * encrypt process
+    // * first generate a salt (str) to encrypt user password
+    // * second generate a hash (password already encrypt)
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+    nextCallback();
+
+})
+
+// * Define methods
+
+schema.methods.comparePasswords = async function(password: string): Promise<boolean> {
+    const user = this;
+    return await bcrypt.compare(password, user.password);
+}
 
 export default model<IUser>("User", schema);
